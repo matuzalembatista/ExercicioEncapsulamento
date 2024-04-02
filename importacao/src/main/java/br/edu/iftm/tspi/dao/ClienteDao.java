7c7f05309a7eae12ca4dd4ca9e11e0847947ff85
YOUR_EMAIL <carloseduardoxpto@gmail.com>
2024-04-01 21:24:27 -0300

    Inclusão do acesso ao banco de dados

--- importacao/src/main/java/br/edu/iftm/tspi/dao/ClienteDao.java
+++ importacao/src/main/java/br/edu/iftm/tspi/dao/ClienteDao.java
@@ -1,12 +1,69 @@
 package br.edu.iftm.tspi.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.mysql.cj.protocol.Resultset;
+
 import br.edu.iftm.tspi.domain.Cliente;
 
 public class ClienteDao {
 
-    public void salvarCliente(Cliente cliente) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'salvarCliente'");
+
+
+    public void salvarCliente(Cliente cliente) throws Exception {
+        Connection connection = Conexao.getConnection();
+        String sql = "INSERT INTO tbCliente(cpfcli,nomcli,"+
+                     "endcli,baicli,cidcli,sigest,datcad) "+
+                     " values (?,?,?,?,?,?,now())";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setString(1,cliente.getCpf());
+        ps.setString(2,cliente.getNome());
+        ps.setString(3,cliente.getEndereco());
+        ps.setString(4,cliente.getBairro());
+        ps.setString(5,cliente.getCidade());
+        ps.setString(6,cliente.getEstado());
+        ps.execute();
+    }
+
+    public void atualizarCliente(Cliente cliente) throws Exception {
+        Connection connection = Conexao.getConnection();
+        String sql = "UPDATE tbCliente set nomcli = ?,"+
+                     "endcli = ?,baicli = ?,cidcli = ?, "+
+                     "sigest = ? where cpfcli = ?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setString(1,cliente.getNome());
+        ps.setString(2,cliente.getEndereco());
+        ps.setString(3,cliente.getBairro());
+        ps.setString(4,cliente.getCidade());
+        ps.setString(5,cliente.getEstado());
+        ps.setString(6,cliente.getCpf());
+        ps.execute();
+    }
+
+    public void persistir(Cliente cliente) throws Exception {
+        if (cliente.getInclusaoAlteracao().equals("I")) {
+            salvarCliente(cliente);
+        } else if (cliente.getInclusaoAlteracao().equals("A")) {
+            atualizarCliente(cliente);
+        } else {
+            throw new Exception("desconheço essa opção de inclusão alteração:"
+                                +cliente.getInclusaoAlteracao());
+        }
+    }
+
+    public Integer getUltimoLote() throws Exception {
+       Connection conexao = Conexao.getConnection();
+       String sql = "select MAX(numlot) as maxLote "+ 
+                    "from tbControleRecebimento "+
+                    "where tiparq = 'CLI'";
+       PreparedStatement ps = conexao.prepareStatement(sql);
+       ResultSet rs = ps.executeQuery();
+       if (rs.next()) {
+          return rs.getInt(1);
+       }
+       throw new Exception("Não encontrei o último lote de cliente");
     }
 
 }
--- importacao/src/main/java/br/edu/iftm/tspi/service/ProcessaCliente.java
+++ importacao/src/main/java/br/edu/iftm/tspi/service/ProcessaCliente.java
@@ -17,38 +17,55 @@ public class ProcessaCliente {
 
     public ProcessaCliente(String path) {
         this.path = path;
+        clienteDao = new ClienteDao();
     }
 
     public void processa() throws Exception {
         BufferedReader br = new BufferedReader(new FileReader(path));
         while (br.ready()) {
             String linha = br.readLine();
-            if (linha.substring(1, 1).equals("2")) {
-                Cliente cliente = getCliente(linha);
-                System.out.println(cliente);
-                clienteDao.salvarCliente(cliente);
+            String opcao = linha.substring(0, 1);
+            if (opcao.equals("2")) {
+                processaDetalhe(linha);
+            } else if (opcao.equals("1")) {
+                processaCabecalho(linha);
+            } else {
+                throw new Exception("Desconheço essa opção de processar a linha: "+opcao);
             }
         }
         br.close();
     }
 
+    private void processaCabecalho(String linha) throws Exception {
+        Integer lote = Integer.parseInt(linha.substring( 1, 4));
+        Integer loteBanco = clienteDao.getUltimoLote();
+        Integer loteEsperado = loteBanco + 1;
+        if (!lote.equals(loteEsperado)) {
+            throw new Exception("Lote recebido: "+lote+ 
+                                "diferente do lote esperado:"+loteEsperado);
+        }
+    }
+
+    private void processaDetalhe(String linha) throws Exception {
+        Cliente cliente = getCliente(linha);
+        clienteDao.persistir(cliente);
+    }
+
     private Cliente getCliente(String linha) throws ParseException {
         Cliente cliente = new Cliente();
-        cliente.setInclusaoAlteracao(linha.substring(2,2));
-        cliente.setCpf(linha.substring(3,13));
-        cliente.setNome(linha.substring(14,43));
-        cliente.setEndereco(linha.substring(44,73));
-        cliente.setBairro(linha.substring(74,103));
-        cliente.setCidade(linha.substring(104,133));
-        cliente.setEstado(linha.substring(134, 135));
+        cliente.setInclusaoAlteracao(linha.substring(1,2));
+        cliente.setCpf(linha.substring(2,13));
+        cliente.setNome(linha.substring(13,43));
+        cliente.setEndereco(linha.substring(43,73));
+        cliente.setBairro(linha.substring(73,103));
+        cliente.setCidade(linha.substring(103,133));
+        cliente.setEstado(linha.substring(133, 135));
         SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyyhhmmss");
-        String dataCadastro = linha.substring(136, 143);
-        String horaCadastro = linha.substring(144, 149);
+        String dataCadastro = linha.substring(135, 143);
+        String horaCadastro = linha.substring(143, 149);
         Date dataHoraCadastro = sdf.parse(dataCadastro+horaCadastro);
         cliente.setDataHoraCadastro(dataHoraCadastro);
         return cliente;
     }
-
-
 
 }
